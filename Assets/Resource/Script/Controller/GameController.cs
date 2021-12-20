14da89e5f8bd2c72470301b08d1b0f8cc633ce50
Dat-GG <lethanhdat160796@gmail.com>
2021-12-21 01:08:35 +0700

    HP Gamecontroller Button

---
+++ Assets/Resource/Script/Controller/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LTAUnityBase.Base.DesignPattern;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public Text score;
+    public int scorenumber;
+    public GameObject Location;
+    public GameObject enemy;
+    public Text Lv;
+    public int Lvnumber;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observer.Instance.AddObserver(TOPICNAME.ENEMYDESTROY, NewEnemy);
+    }
+    private void OnDestroy()
+    {
+        Observer.Instance.RemoveObserver(TOPICNAME.ENEMYDESTROY, NewEnemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "" + scorenumber.ToString();
+        Lv.text = "" + Lvnumber.ToString();
+    }
+    public void NewEnemy(object data)
+    {
+        Instantiate(enemy, Location.transform.position, Location.transform.rotation);
+    }
+}
---
+++ Assets/Resource/Script/Controller/HPController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPController : MonoBehaviour
+{
+    public Slider HP;
+    SpriteRenderer spriterender;
+    public int HPPlayer;
+    public static HPController instance;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP.maxValue = HPPlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HP.value = HPPlayer;
+    }
+    public void TruMau(int dame)
+    {
+        HPPlayer -= dame;
+    }
+
+}
